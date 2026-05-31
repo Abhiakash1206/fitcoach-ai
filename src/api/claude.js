6d2a0df283e2bbe9abe0d092e21083e0e5d799e4
Abhiakash1206 <abhiakash1206@gmail.com>
2026-06-01 01:11:49 +0530

    fix: update Gemini model

--- src/api/claude.js
+++ src/api/claude.js
@@ -8,7 +8,7 @@ export async function sendMessage(messages) {
   const lastMsg = messages[messages.length - 1].content;
 
   const response = await fetch(
-  "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-04-17:generateContent?key=" + import.meta.env.VITE_GEMINI_API_KEY, {
+"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite-preview-06-17:generateContent?key=" + import.meta.env.VITE_GEMINI_API_KEY, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
